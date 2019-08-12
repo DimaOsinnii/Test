2f51490fcdbf7706d12244646d3bd634f293ef7e
DimaOsinnii <ozerland@gmail.com>
2019-08-12 20:51:23 +0300

    add into the project imagemin & fix some bugs

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,15 @@ const cleanCSS = require('gulp-clean-css');//write css in one line
 const browserSync = require('browser-sync').create();//local server
 const autoprefixer = require('gulp-autoprefixer');//Add prefix for new properties
 const  bable = require('gulp-babel');//compiler js ES6 into ES5
+const imagemin = require('gulp-imagemin');
 
+function images() {
+    return pipeline (
+        src('./app/images/*'),
+        (imagemin()),
+        (dest('./build/images'))
+    )
+}
 
 function addStyles(paths, outputFilename) {
     return pipeline(
@@ -68,9 +76,10 @@ function watcher() {
     watch("/*.html").on('change', browserSync.reload);
 }//tracking all files that can be changed for hot reload with Browser Sync
 
+task('images', images);
 task('styles', styles);
 task('scripts', scripts);
 task('del', clean);
 task('watch', watcher);
-task('build', series(clean, parallel(styles, scripts)));
+task('build', series(clean, parallel(images, styles, scripts)));
 task('dev', series('build', watcher));
